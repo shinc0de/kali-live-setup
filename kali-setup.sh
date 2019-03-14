a7286c9c488d2de6f860c483d278890916598680
root <root@localhost.localdomain>
2019-03-14 20:24:44 +0000

    add shortcut to open terminal

--- kali-setup.sh
+++ kali-setup.sh
@@ -4,7 +4,13 @@
 setxkbmap de &&
 gsettings set org.gnome.desktop.sound event-sounds false &&
 
+# bind ctrl+alt+t to new terminal
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ name 'new terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
+
 # set custom resolution
-xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
-xrandr --addmode Virtual-1 2560x1440_60.00 &&
-xrandr --output Virtual-1 --mode "2560x1440_60.00"
+#xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
+#xrandr --addmode Virtual-1 2560x1440_60.00 &&
+#xrandr --output Virtual-1 --mode "2560x1440_60.00"
