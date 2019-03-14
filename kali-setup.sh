f2e266c1df1007e8e9da0a6aeedcf3c0249b7462
root <root@localhost.localdomain>
2019-03-14 20:30:04 +0000

    removed comment

--- kali-setup.sh
+++ kali-setup.sh
@@ -11,6 +11,6 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
 
 # set custom resolution
-#xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
-#xrandr --addmode Virtual-1 2560x1440_60.00 &&
-#xrandr --output Virtual-1 --mode "2560x1440_60.00"
+xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
+xrandr --addmode Virtual-1 2560x1440_60.00 &&
+xrandr --output Virtual-1 --mode "2560x1440_60.00"
