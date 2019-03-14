7bb3fd59d263f8acbf5ce9d73e7e3148057cd570
root <root@localhost.localdomain>
2019-03-14 18:47:04 +0000

    changed shebang made cmds execute only if previous one finished successfully

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 # set keyboard layout
-setxkbmap de 
+setxkbmap de &&
 
 # use this command to get the settings for your 
 # monitor resolution 
 # cvt 2560 1440
 
 # set custom resolution
-xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync
-xrandr --addmode Virtual-1 2560x1440_60.00
+xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
+xrandr --addmode Virtual-1 2560x1440_60.00 &&
 xrandr --output Virtual-1 --mode "2560x1440_60.00"
