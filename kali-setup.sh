2a73ff471700c43c4c53a807a4606519fddbb340
root <root@localhost.localdomain>
2019-03-17 09:09:31 +0000

    "update"

--- kali-setup.sh
+++ kali-setup.sh
@@ -12,5 +12,5 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 
 # set custom resolution
 xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
-xrandr --addmode Virtual-1 2560x1440_60.00 &&
-xrandr --output Virtual-1 --mode "2560x1440_60.00"
+xrandr --addmode Virtual-0 2560x1440_60.00 &&
+xrandr --output Virtual-0 --mode "2560x1440_60.00"
