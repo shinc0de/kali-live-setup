01893022c08a1a4f942ad6825978983787495d1a
root <root@localhost.localdomain>
2019-03-17 14:40:21 +0000

    "update"

--- kali-setup.sh
+++ kali-setup.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 cp ~/kali-live-setup/bash_aliases ~/.bash_aliases
 cp ~/kali-live-setup/vimrc ~/.vimrc
-source ~/.bashrc
+source /root/.bashrc
 
 # set keyboard layout
 setxkbmap de &&
@@ -13,9 +13,10 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
 
-output_device = xrandr --current | grep "Virtual" | cut -f1 -d ' '
+OUTPUT_DEVICE="$(xrandr --current | grep "Virtual" | cut -f1 -d ' ')";
 
 # set custom resolution
-xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
-xrandr --addmode $output_device 2560x1440_60.00 &&
-xrandr --output $output_device --mode "2560x1440_60.00"
+xrandr --newmode "2560x1440_60.00" 312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
+xrandr --addmode ${output_device} 2560x1440_60.00 &&
+#xrandr --output ${output_device} --mode "1920x1080"
+xrandr --output ${output_device} --mode "2560x1440_60.00"
