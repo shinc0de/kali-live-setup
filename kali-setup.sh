df30f26163cca7cb3d2cff686589fbccec8d1565
root <root@localhost.localdomain>
2019-03-17 11:45:44 +0000

    "update"

--- kali-setup.sh
+++ kali-setup.sh
@@ -10,7 +10,9 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ command 'gnome-terminal'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/ binding '<Control><Alt>t'
 
+output_device = xrandr --current | grep "Virtual" | cut -f1 -d ' '
+
 # set custom resolution
 xrandr --newmode "2560x1440_60.00"  312.25  2560 2752 3024 3488  1440 1443 1448 1493 -hsync +vsync &&
-xrandr --addmode Virtual-0 2560x1440_60.00 &&
-xrandr --output Virtual-0 --mode "2560x1440_60.00"
+xrandr --addmode $output_device 2560x1440_60.00 &&
+xrandr --output $output_device --mode "2560x1440_60.00"
